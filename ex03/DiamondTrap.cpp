bcfb8878ca98b61ba15c0f3cc676ef433c504283
Crealex <Alexandre.tomasi80@gmail.com>
2025-05-08 09:58:48 +0200

    advancing exo3

--- ex03/DiamondTrap.cpp
+++ ex03/DiamondTrap.cpp
@@ -1,19 +1,31 @@
 
+#include "ClapTrap.hpp"
+#include "FragTrap.hpp"
+#include "ScavTrap.hpp"
 #include "colors.h"
 #include "DiamondTrap.hpp"
 
 DiamondTrap::DiamondTrap(): _name("Default FragTrap"), FragTrap(), ScavTrap()
 {
 	ClapTrap::_name = _name + "_clap_name";
-	_hitPoints = FragTrap::_hitPoints;
-	_attackDamage = FragTrap::_attackDamage;
+	this->_hitPoints = FragTrap::_hitPoints;
+	this->_attackDamage = FragTrap::_attackDamage;
 	std::cout << GREEN << "Default DiamondTrap constructor called" << RESET << std::endl;
 }
 
-
-
-DiamondTrap::DiamondTrap(const DiamondTrap &cpy)
+DiamondTrap::DiamondTrap(std::string name): _name(name), FragTrap(), ScavTrap()
 {
+	ClapTrap::_name = _name + "_clap_name";
+	this->_hitPoints = FragTrap::_hitPoints;
+	this->_attackDamage = FragTrap::_attackDamage;
+	std::cout << LIGHT_GREEN << "DiamondTrap constructor called with the name " << ITALIC << this->_name << RESET << std::endl;
+}
+
+DiamondTrap::DiamondTrap(const DiamondTrap &cpy): ScavTrap(cpy._name)
+{
+	this->_attackDamage = cpy._attackDamage;
+	this->_hitPoints = cpy._hitPoints;
+	this->_energyPoints = cpy._energyPoints;
 	std::cout << BLUE << "Copy constructor DiamondTrap called" << RESET << std::endl;
 }
 
@@ -24,6 +36,11 @@ DiamondTrap::~DiamondTrap()
 
 DiamondTrap &DiamondTrap::operator=(const DiamondTrap &rhs)
 {
-
+	this->_name = rhs._name;
+	ClapTrap::_name = rhs._name + "_clap_name";
+	this->_attackDamage = rhs._attackDamage;
+	this->_hitPoints = rhs._hitPoints;
+	this->_energyPoints = rhs._energyPoints;
+	std::cout << "Assignment operator DiamdonTrap called for " << ITALIC << this->_name << RESET << std::endl;
+	return (*this);
 }
-
